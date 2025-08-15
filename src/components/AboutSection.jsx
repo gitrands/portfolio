c5f466a0f7c798d3adb59ed8b58ce33bc640c59b
jeeva <jeevara2002@gmail.com>
2025-08-15 13:53:45 +0530

    Add Resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -62,10 +62,9 @@ export const AboutSection = () => {
                   <User className="h-6 w-6 text-primary" />
                 </div>
                 <div className="text-left">
-                  <h4 className="font-semibold text-lg">UI/UX Design</h4>
+                  <h4 className="font-semibold text-lg">Java developer</h4>
                   <p className="text-muted-foreground">
-                    Designing intuitive user interfaces and seamless user
-                    experiences.
+                    Designing intuitive web applications with Spring boot and Microservices .
                   </p>
                 </div>
               </div>
